4e7f684c8a3c2d05a9b91676762fd8e3c4e23fb1
nida1-lab <nida.89006.284@gmail.com>
2026-09-20 18:32:17 +0900

    Initialize build.gradle.kts for Android project

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.nida.bench"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.nida.bench"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
